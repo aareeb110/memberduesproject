93992d4f1c14e5458451c14d24bec5fc1b7bc8c7
Areeb Amjad <aareeb990@gmail.com>
2021-01-25 18:16:41 -0800

    Created csv_writer to write member and payment status info to Member Dues List and to convert this list into an Excel Workbook. send_reminders reads from the workbook and stores email addresses of members who have not paid into a list.

--- send_reminders.py
+++ send_reminders.py
@@ -1,1 +1,49 @@
-print("hello world!")
+import xlrd
+
+file_path = "Member Dues.xlsx"
+wb = xlrd.open_workbook(file_path)
+sheet = wb.sheet_by_index(0)
+
+email_list = []
+
+header = []
+for col in range(sheet.ncols):
+    header.append(sheet.cell_value(0, col))
+
+list_of_rows = []
+for row in range(1, sheet.nrows):
+    rows = {}
+    for col in range(sheet.ncols):
+        rows[header[col]] = sheet.cell_value(row, col)
+    list_of_rows.append(rows)
+
+for i in list_of_rows:
+    if i['Paid (Y/N)'] == 'N':
+        email_list.append(i['Email'])
+
+
+
+
+
+
+
+
+
+
+#
+# # First store members' paid statuses in a list
+#
+# for i in range(1, sheet.nrows):
+#     if sheet.cell_value(i, 5) == 'Y':
+#         has_paid_list.append(True)
+#     if sheet.cell_value(i, 5) == 'N':
+#         has_paid_list.append(False)
+#
+# for i, j in enumerate(has_paid_list):
+#     if not j:
+#         email_list.append(sheet.cell_value(i + 1, 4))
+
+
+
+
+
